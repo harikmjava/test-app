6f820967dd3e34247743582e3edae02bfd990a0c
Harishkumar Muddasani <harishmuddasa@gmail.com>
2023-03-06 16:46:42 -0600

    Remove import react and remove React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import React from 'react';
 //In modern react we do not need import React, 
 //however when JSX compiling in to calls React.createElement in olde react we need it
 //
 
 export default function App(){
    return (
-    <React.Fragment>
+    <>
       <h1>Hello World</h1>
       <p>Test</p>   
-    </React.Fragment>  
+    </>  
     );
 }
