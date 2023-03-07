46949871e7c0307743807235a716d9071d029ddd
Harishkumar Muddasani <harishmuddasa@gmail.com>
2023-03-06 22:45:10 -0600

    override different types of events

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-//In modern react we do not need import React, 
+import "./App.css";
+//In modern react we do not need import React,
 //however when JSX compiling in to calls React.createElement in olde react we need it
 //
 
-export default function App(){
-   return (
-    <>
-      <h1>Hello World</h1>
-      <p>Test</p>   
-    </>  
-    );
+export default function App() {
+
+  const handleClick = () => {
+    console.log('clicked');
+  };
+
+  return (
+    <button
+      onClick={() => {
+        console.log("clicked");
+      }}
+    >
+      Click Me
+    </button>
+  );
 }
+
+function handleClick() {
+  console.log("clicked1");
+};
+
